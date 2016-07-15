68f5e0fccd83e8b140cc9ae731b6e7c64f5cefec
Martin Mastny <mastnym@vscht.cz>
2016-07-15 10:16:57 +0200

    tinymce plugins

---
+++ lib/editor/tinymce/plugins/insertdollars/lang/en/tinymce_insertdollars.php
@@ -0,0 +1,7 @@
+<?php
+
+$string['pluginname'] = 'Insert dollars';
+$string['insertdollars:button'] = 'Insert markup for chemical formula';
+
+//able to send params this way
+$string['insertdollars:delta_width'] = 1000;

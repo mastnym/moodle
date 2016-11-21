e8ae14a40c56e2f68383082ceada853af26c30f6
Martin Mastny <mastnym@vscht.cz>
2016-11-21 16:19:14 +0100

    lightbox added to ceratin courses

---
+++ cis/js/lightboxcontent.js
@@ -0,0 +1,14 @@
+var course_ids = [59, 104, 109, 110];
+var selector = "body.course-{{course_id}} div[role=main] img"
+var selectors = [];
+$.each(course_ids, function(index, value){
+   selectors.push(selector.replace("{{course_id}}",value)); 
+});
+
+$(document).ready(function(){
+   $(selectors.join(",")).click(function(){
+       $(this).lightbox_me(); 
+   }).hover(function(){
+       $(this).css("cursor", "pointer"); 
+   });
+});

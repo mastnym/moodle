b959582b2b06bad98c22dbee719876757e9c4418
Martin Mastny <mastnym@vscht.cz>
2016-11-21 10:58:17 +0100

    login allows non shib users

--- cis/shiblogin/login.js
+++ cis/shiblogin/login.js
@@ -1,6 +1,13 @@
-
-$(".loginsub").after('<div id="shiblogin" class="subcontent ">');
 var lang = $('html').attr('lang');
+var novscht = lang == "en" ? "I use other email address to login" : "Pro přihlášení používám jinou emailovou adresu";
+$(".loginsub").before('<div id="shiblogin" class="subcontent "/><div class="subcontent"><a href="#" id="show-regular-login">'+novscht +'</a></div>');
+
 var login_text = lang == "en" ? "Login with ICT account(@vscht.cz)" : "Přihlásit se pomocí údajů VŠCHT Praha"
 var shib_form = $('<form method="GET" action="/auth/shibboleth/index.php"><input type="submit" class="form-submit" value="'+login_text+'"/></form>');
 $("#shiblogin").append(shib_form);
+$(document).ready(function(){
+    $("#show-regular-login").click(function(){
+        $(".loginsub").toggle();
+        return false;
+    });
+})
